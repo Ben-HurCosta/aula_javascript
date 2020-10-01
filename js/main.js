ce22d7b15437f2effd8965be0e90f25000fc069d
Ben-Hur Costa <benhur.costa89@gmail.com>
2020-09-30 21:47:25 -0300

    enviando arquivos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert("Meu primeiro JS");
+var name = "Ben-Hur Costa";
+var idade = 30;
+var addidade = 1
+var idade2 = idade + addidade
+//alert("Bem vindo " + name + " Tem " + idade2  +  " anos ");
+console.log(idade+addidade);
+var frase = "Japão é o melhor time do mundo";
+console.log(frase.replace("Japão","Brasil"));
+//da para usar o alert tbm
+console.log(frase.toLocaleUpperCase());
+console.log(frase.toLowerCase());
+n1 = 5;
+n2 = 3;
+alert(n1*n2);
